87f061e64a3cf4f8ee033a85b360f4dd4f33e995
FrankDestro <frank-destro@outlook.com>
2022-11-27 20:21:08 -0300

    initial configuration project

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,68 +1,25 @@
-import { useEffect, useMemo, useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Filter from './components/filter';
-import Header from './components/header';
-import PieChartCard from './components/pie-chart-card';
-import SalesByDate from './components/sales-by-date';
-import SalesSummary from './components/sales-summary';
-import SalesTable from './components/sales-table';
-import { buildSalesByPaymentMethod, buildSalesByStoreChart } from './helpers';
-import { FilterData, PieChartConfig, SalesByPaymentMethod, SalesByStore } from './types';
-import { buildFilterParams, makeRequest } from './utils/request';
 
 function App() {
-  const [filterData, setFilterData] = useState<FilterData>();
-  const [salesByStore, setSalesByStore] = useState<PieChartConfig>();
-  const [salesByPaymentMethod, setSalesByPaymentMethod] = useState<PieChartConfig>();
-
-  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
-
-  useEffect(() => {
-    makeRequest
-      .get<SalesByStore[]>('/sales/by-store', { params })
-      .then((response) => {
-        const newSalesByStore = buildSalesByStoreChart(response.data);
-        setSalesByStore(newSalesByStore);
-      })
-      .catch(() => {
-        console.error('Error to fetch sales by store');
-      });
-  }, [params]);
-
-  useEffect(() => {
-    makeRequest
-      .get<SalesByPaymentMethod[]>('/sales/by-payment-method', { params })
-      .then((response) => {
-        const newSalesByPaymentMethod = buildSalesByPaymentMethod(response.data);
-        setSalesByPaymentMethod(newSalesByPaymentMethod);
-      })
-      .catch(() => {
-        console.error('Error to fetch sales by payment method');
-      });
-  }, [params]);
-
-  const onFilterChange = (filter: FilterData) => {
-    setFilterData(filter);
-  };
-
   return (
-    <>
-      <Header />
-      <div className="app-container">
-        <Filter onFilterChange={onFilterChange} />
-        <SalesByDate filterData={filterData} />
-        <div className="sales-overview-container">
-          <SalesSummary filterData={filterData} />
-          <PieChartCard name="Lojas" labels={salesByStore?.labels} series={salesByStore?.series} />
-          <PieChartCard
-            name="Pagamento"
-            labels={salesByPaymentMethod?.labels}
-            series={salesByPaymentMethod?.series}
-          />
-        </div>
-        <SalesTable filterData={filterData} />
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
